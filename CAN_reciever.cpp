9817cd75ccb48caf77fcae7fb34e85244a20b19e
thenipplephilosophy <cameronrsbh@googlemail.com>
2015-06-09 12:23:21 +0100

    Removed a lot of crap for a display
    Should now be a little easier to see what is going on

--- CAN_reciever.cpp
+++ CAN_reciever.cpp
@@ -4,8 +4,6 @@
 #include "N5110.h"
 
 Serial pc(USBTX, USBRX);
-//      VCC,SCE,RST,D/C,MOSI,SCLK,LED
-N5110 lcd(p11,p13,p14,p12,p5,p7,p21);
 Ticker ticker;
 Timeout pollChange;
 DigitalOut led1(LED1);
@@ -15,8 +13,6 @@ CAN can1(p30, p29);
 char counter = 0;
 char temp;
 float x;
-char lcdContrast = 0x19;
-bool screenBuffer[84][48];
 unsigned int engineTemp = 0;
 unsigned int mph = 0;
 
@@ -44,7 +40,6 @@ void CANRecivedMessage(CANMessage msg){
 
 
 int main() {
-    lcd.init();
     pc.baud(115200);
     led2 = 1;
     CANMessage msg;
@@ -57,21 +52,8 @@ int main() {
             CANRecivedMessage(msg);
             led2 = !led2;
             
-            // these are default settings so not strictly needed
-            lcd.normalMode();      // normal colour mode
-            lcd.setBrightness(0.5); // put LED backlight on 50%
-            
             if(msg.id == 10){engineTemp = msg.data[0];}
-            if(msg.id == 700){mph = msg.data[0];}
-            
-            char buffer[14];
-            memset(&buffer, 0, 14);
-            lcd.clear();
-            int length = sprintf(buffer,"engine: %d", engineTemp);
-            lcd.printString(buffer,0,0);
-            sprintf(buffer,"speed: %dmph", mph);
-            lcd.printString(buffer,0,1);
-        
+            if(msg.id == 700){mph = msg.data[0];}        
  
         }
     }
