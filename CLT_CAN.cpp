edf61ffafcd26dea2b186517712a90bfc490259b
mtag123 <martingreer98@gmail.com>
2015-06-26 13:05:03 +0100

    Change send rate option, name fixes
    Added the ability to change the send rate of the node and did some
    renaming to make things more readable.

--- CLT_CAN.cpp
+++ CLT_CAN.cpp
@@ -17,12 +17,23 @@
 #define B 0.000239245801500490
 #define C 0.00000012708217191349288
 
+#define DEFAULT_SEND_RATE 1
+#define CAN_ID 10
+#define MESSAGE_LENGTH 5
+#define FLOAT_BIT 0x81
+
 DigitalOut led1(LED1);
+DigitalOut led2(LED2);
 Ticker ticker;
 CAN can(p30, p29);
 AnalogIn sensor(p20);
 
-//Calcuates temperature from an analog value
+union fc {
+        float f;
+        char c[4];
+};
+
+//Calculates temperature from an analog value
 float getTemperature()
 {
     float analog_in = 0;
@@ -44,24 +55,44 @@ float getTemperature()
 //Writes a message to the CAN bus
 void send()
 {
-    char temperature = (int)getTemperature();
-    char msg[2] = {0x80, temperature}; //0x80 means info message
-    if (can.write(CANMessage(10, msg, 2))) {
+    fc temperature;
+    temperature.f = getTemperature();
+    char data[MESSAGE_LENGTH] = {FLOAT_BIT, temperature.c[0], temperature.c[1], temperature.c[2], temperature.c[3]};
+    if (can.write(CANMessage(CAN_ID, data, MESSAGE_LENGTH))) {
         led1 = !led1;
     }
 }
 
 void setSendRate(float sendRate){
     ticker.attach(&send, sendRate);
+}
+
+void receivedMessage(CANMessage msg) {
+
+    //If first byte is device's CAN ID then the next 4 bits are new send rate
+    if (msg.data[0] == CAN_ID) {
+        fc newSendRate;
+
+        newSendRate.c[0] = msg.data[1];
+        newSendRate.c[1] = msg.data[2];
+        newSendRate.c[2] = msg.data[3];
+        newSendRate.c[3] = msg.data[4];
+
+        setSendRate(newSendRate.f);
     }
-    
 
-void initialise() {
-    ticker.attach(&send, 1);  //Default sends CAN message every second
 }
 
-int main()
-{
-    initialise();
-    while(1);
+
+int main() {
+    ticker.attach(&send, DEFAULT_SEND_RATE);
+    CANMessage msg;
+    while(1) {
+        if (can.read(msg)) {
+            led2 = !led2;
+            receivedMessage(msg);
+        }
+        wait(0.001);
+    }
 }
+
